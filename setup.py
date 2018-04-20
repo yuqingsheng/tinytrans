0f0a402a93921dcebced8d13e760269ccd703d5b
yuqingsheng <qingsheng.yu@ishansong.com>
2018-04-20 18:32:43 +0800

    modify setup

--- setup.py
+++ setup.py
@@ -12,10 +12,9 @@ with open('README.md', 'rb') as f:
     readme = f.read().decode('utf-8')
 
 install_requires = []
-with open('requirements.txt', 'rb') as f:
+with open('requirements.txt', 'r') as f:
     for req in f.readlines():
-        if not req.startswith('git+'):
-            install_requires.append(req.strip())
+        install_requires.append(req.strip())
 
 
 setup(
@@ -30,12 +29,10 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
     ],
-    author='qingsheng.yu@ishansong.com',
+    author='yuqingsheng',
+    author_email='qingsheng.yu@ishansong.com',
     url='http://www.yongqianbao.com',
     packages=find_packages(),
-    package_data = {
-        'maat.conf' : ['*'],
-    },
     include_package_data=True,
     zip_safe=False,
 )
